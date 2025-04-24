edb11fded1da162576a6203703ccd896f459ec1f
qqpprun <qqpprun84fe@AtomGit.com>
2025-04-24 15:29:34 +0800

    fix

--- acl_ner_dev/code_dev/.ipynb_checkpoints/eval_dev-checkpoint.sh
+++ acl_ner_dev/code_dev/.ipynb_checkpoints/eval_dev-checkpoint.sh
@@ -1,14 +1,14 @@
-# python score.py --file_name v12 --modelname o3-mini
-# python score.py --file_name v21 --modelname o3-mini
-# python score.py --file_name v22 --modelname o3-mini
+python score.py --file_name v12 --modelname o3-mini
+python score.py --file_name v21 --modelname o3-mini
+python score.py --file_name v22 --modelname o3-mini
 
-# python score.py --file_name v12 --modelname gemini-2.0-flash
-# python score.py --file_name v21 --modelname gemini-2.0-flash
-# python score.py --file_name v22 --modelname gemini-2.0-flash
+python score.py --file_name v12 --modelname gemini-2.0-flash
+python score.py --file_name v21 --modelname gemini-2.0-flash
+python score.py --file_name v22 --modelname gemini-2.0-flash
 
-# python score.py --file_name v12 --modelname deepseek-v3-250324
-# python score.py --file_name v21 --modelname deepseek-v3-250324
-# python score.py --file_name v22 --modelname deepseek-v3-250324
+python score.py --file_name v12 --modelname deepseek-v3-250324
+python score.py --file_name v21 --modelname deepseek-v3-250324
+python score.py --file_name v22 --modelname deepseek-v3-250324
 
 python score.py --file_name 1 --modelname stack
 python score.py --file_name 2 --modelname stack
